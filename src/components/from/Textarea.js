15935665dfb27cae43d44e6052a3929ccb9fc366
sonipalak <online.palak@gmail.com>
2023-06-25 18:21:18 +0530

    Update Textarea

--- src/components/from/Textarea.js
+++ src/components/from/Textarea.js
@@ -4,16 +4,16 @@ import React, { useState } from "react";
 function Textarea(props) {
 
     const [text, setText] = useState(`Enter Your Massage`);
-    const [characterCount, setcharacterCount] = useState(0);
 
     const heandleUpClick = () => {
+        //console.log("Update on Click" + text);
         let newText = text.toUpperCase();
         setText(newText)
     }
 
     const heandleonChange = (event) => {
+        //console.log("Update On Change");
         setText(event.target.value);
-        setcharacterCount(event.target.value.length);
     }
 
 
@@ -23,7 +23,7 @@ function Textarea(props) {
             <label className='form-field__label'>{props.label}</label>
             <div className='form-field__textarea-wrap'>
                 <textarea id="myBox" onChange={heandleonChange} value={text} rows={props.rows}> </textarea>
-                <p>Character Count: {characterCount}</p>
+                <p>Word Count: {text.split(" ").length}, Character Count: {text.length}</p>
             </div>
             <button onClick={heandleUpClick}>Update Text</button>
         </div>
