5801067a4a5562a12d75222a5c03f2daa1647bb5
sonipalak <online.palak@gmail.com>
2023-06-26 17:58:47 +0530

    Update Mode

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import { useState } from 'react';
 //import Contact from './components/Contact';
 import About from './components/About';
 import Navigation from './components/Navigation';
+import Notification from './components/Notification';
 
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (Massage, type) => {
+    setAlert({
+      msg: Massage,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000)
+  }
   const toggleMode = () => {
     if(mode === 'light'){
       setMode('dark');
-      document.body.style.backgroundColor = '#212121'
+      document.body.style.backgroundColor = '#212121';
+      showAlert("Dark Mode has been enabled", "success");
     }
     else{
       setMode('light');
-      document.body.style.backgroundColor = '#ffffff'
+      document.body.style.backgroundColor = '#ffffff';
+      showAlert("Light Mode has been enabled", "success");
     }
   }
   return (
     <div className="App">
       <Navigation mode={mode} toggleMode={toggleMode} />
+      <Notification alert={alert}/>
       <About title="This is the About Page" mode={mode} />
       {/* <Contact /> */}
     </div>
