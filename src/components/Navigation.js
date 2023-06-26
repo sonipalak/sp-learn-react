6a4154f30cb4173b94a0a1ff1c2769d32d08f5ea
sonipalak <online.palak@gmail.com>
2023-06-27 01:41:25 +0530

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import PropsType from 'prop-types';
 
 function Navigation(props) {
     return (
